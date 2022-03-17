a9407712130117b5528b3ef9199d9e77836eec00
John F. Lake Jr <john.f.lake.jr@gmail.com>
2022-03-16 22:01:04 -0400

    Added some assets for the start screens/menu.  Added basic start menu.

--- include/GameEngine.hpp
+++ include/GameEngine.hpp
@@ -3,13 +3,19 @@
 
 #include <SDL.h>
 
+#define MENU_NEW 0
+#define MENU_CONTINUE 1
+#define MENU_EXIT 2
+
 class GameEngine {
 
   SDL_Window * game_window;
   SDL_Renderer * game_renderer;
 
   void game_loop();
+  SDL_Texture * load_texture(std::string);
   bool present_static_screen(std::string);
+  int present_start_menu();
 
   public: 
     bool initialize_sdl();
--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -58,6 +58,89 @@ bool GameEngine::initialize_sdl() {
 
 }
 
+SDL_Texture * GameEngine::load_texture(std::string path) {
+  SDL_Surface * temp = IMG_Load(path.c_str());
+  return SDL_CreateTextureFromSurface(game_renderer, temp);
+}
+
+/**
+ * @brief Present the start menu and get the resulting choice from the user.
+ * 
+ * @return int choice (New Game / Continue / Exit Game)
+ */
+int GameEngine::present_start_menu() {
+
+  SDL_Texture * menu_background_texture = load_texture("./assets/menu_background.png");
+  SDL_Texture * new_game_button_selected = load_texture("./assets/new_game_button_selected.png");
+  SDL_Texture * new_game_button_unselected = load_texture("./assets/new_game_button_unselected.png");
+  SDL_Texture * continue_button_selected = load_texture("./assets/continue_button_selected.png");
+  SDL_Texture * continue_button_unselected = load_texture("./assets/continue_button_unselected.png");
+  SDL_Texture * exit_game_button_selected = load_texture("./assets/exit_game_button_selected.png");
+  SDL_Texture * exit_game_button_unselected = load_texture("./assets/exit_game_button_unselected.png");
+
+  SDL_Rect new_game_button_rect = {220,100,220,60};
+  SDL_Rect continue_button_rect = {220,200,220,60};
+  SDL_Rect exit_game_button_rect = {220,300,220,60};
+
+  int selected = MENU_NEW;
+
+  SDL_Event e;
+  bool show_screen = true;
+
+  while(show_screen) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (SDL_QUIT == e.type) {
+        return false;
+      } else if (SDL_KEYDOWN == e.type) {
+        switch(e.key.keysym.sym) {
+          case SDLK_DOWN:
+            selected++;
+            if (selected > MENU_EXIT) {
+              selected = MENU_EXIT;
+            }
+            break;
+          case SDLK_UP:
+            selected--;
+            if (selected < MENU_NEW) {
+              selected = MENU_NEW;
+            }
+            break;
+          case SDLK_RETURN:
+            show_screen = false;
+            break;
+        }
+      }
+    }
+
+    SDL_RenderClear(game_renderer);
+    SDL_RenderCopy(game_renderer, menu_background_texture, NULL, NULL);
+
+    switch(selected) {
+      case MENU_NEW:
+        SDL_RenderCopy(game_renderer, new_game_button_selected, NULL, &new_game_button_rect);
+        SDL_RenderCopy(game_renderer, continue_button_unselected, NULL, &continue_button_rect);
+        SDL_RenderCopy(game_renderer, exit_game_button_unselected, NULL, &exit_game_button_rect);
+        break;
+      case MENU_CONTINUE:
+        SDL_RenderCopy(game_renderer, new_game_button_unselected, NULL, &new_game_button_rect);
+        SDL_RenderCopy(game_renderer, continue_button_selected, NULL, &continue_button_rect);
+        SDL_RenderCopy(game_renderer, exit_game_button_unselected, NULL, &exit_game_button_rect);
+        break;
+      case MENU_EXIT:
+        SDL_RenderCopy(game_renderer, new_game_button_unselected, NULL, &new_game_button_rect);
+        SDL_RenderCopy(game_renderer, continue_button_unselected, NULL, &continue_button_rect);
+        SDL_RenderCopy(game_renderer, exit_game_button_selected, NULL, &exit_game_button_rect);
+        break;
+    }
+
+    SDL_RenderPresent(game_renderer);
+  }
+
+  SDL_DestroyTexture(menu_background_texture);
+
+  return selected;
+}
+
 /**
  * @brief Show the start screen to the user.  Proceed when a key is pressed. 
  * 
@@ -102,6 +185,12 @@ void GameEngine::play() {
   if (!present_static_screen("./assets/second_screen.png")){
     return;
   }
+
+  int option = present_start_menu();
+  if (MENU_EXIT == option) {
+    return;
+  }
+
   bool game_over = false;
   int step_delay = 2000;
   SDL_Event e;
